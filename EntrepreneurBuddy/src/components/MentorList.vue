50edd50aad7e2cc929a5419a42450053e7fae4f5
Lydia Winters <lydia@dev-squared.com>
2019-10-26 16:01:58 -0500

    filter

--- EntrepreneurBuddy/src/components/MentorList.vue
+++ EntrepreneurBuddy/src/components/MentorList.vue
@@ -1,8 +1,13 @@
 <template>
   <div v-if="loading"><img src="/images/loader.gif" alt=""></div>
+
   <div v-else class="container">
+    <div class="d-flex justify-content-between align-items-center">
+      <h1 class="display-4 name-color mb-4 font-weight-bold text-center">Mentors</h1>
+      <input v-model="filter"></input>
+    </div>
     <div class="row">
-      <div v-for="mentor in mentors" :key="mentor.id" class="col-md-4">
+      <div v-for="mentor in filteredMentors" :key="mentor.id" class="col-md-4">
         <mentor-card rounded :mentor="mentor"></mentor-card>
       </div>
     </div>
@@ -17,11 +22,29 @@
     data: () => ({
       mentors: [],
       loading: false,
+      filter: '',
 
     }),
 
     mounted() {
       this.getMentors();
+    },
+
+    computed: {
+      filteredMentors() {
+        if (this.mentors.length === 0) {
+          return []
+        }
+        let filtered = JSON.parse(JSON.stringify(this.mentors))
+        filtered = filtered.filter(mentor =>
+          mentor.firstName.toLowerCase().includes(this.filter.toLowerCase()) ||
+          mentor.lastName.toLowerCase().includes(this.filter.toLowerCase()) ||
+          mentor.position.toLowerCase().includes(this.filter.toLowerCase()) ||
+          mentor.skills.toLowerCase().includes(this.filter.toLowerCase()) ||
+          mentor.bio.toLowerCase().includes(this.filter.toLowerCase()))
+
+        return filtered
+      }
     },
 
     methods: {
