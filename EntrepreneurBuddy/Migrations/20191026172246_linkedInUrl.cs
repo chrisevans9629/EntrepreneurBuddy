274dc935696038ebba7171a4929e2d828b15c957
Chris Evans <Cevans@peerlessproducts.com>
2019-10-26 12:23:50 -0500

    added linkedin Url

---
+++ EntrepreneurBuddy/Migrations/20191026172246_linkedInUrl.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntrepreneurBuddy.Migrations
+{
+    public partial class linkedInUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LinkedInUrl",
+                table: "Mentors",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LinkedInUrl",
+                table: "Mentors");
+        }
+    }
+}
---
+++ EntrepreneurBuddy/Models/EntrepreneurMentoringRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace EntrepreneurBuddy.Models
+{
+    public class EntrepreneurMentoringRequest
+    {
+        public int Id { get; set; }
+        public int EntreprenuerId { get; set; }
+        public int MentoringRequestId { get; set; }
+        public IList<Entrepenuer>  Entrepenuers { get; set; }
+        public IList<MentoringRequest> MentoringRequests { get; set; }
+    }
+}
