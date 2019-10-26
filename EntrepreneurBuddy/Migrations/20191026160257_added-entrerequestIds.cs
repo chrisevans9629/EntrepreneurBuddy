040828bb7ccbd96b0b686a080e802149b7e6ea8b
Chris Evans <Cevans@peerlessproducts.com>
2019-10-26 11:04:48 -0500

    updated database

---
+++ EntrepreneurBuddy/Migrations/20191026160257_added-entrerequestIds.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntrepreneurBuddy.Migrations
+{
+    public partial class addedentrerequestIds : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EntreprenuerId",
+                table: "EntrepreneurMentoringRequests",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MentoringRequestId",
+                table: "EntrepreneurMentoringRequests",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EntreprenuerId",
+                table: "EntrepreneurMentoringRequests");
+
+            migrationBuilder.DropColumn(
+                name: "MentoringRequestId",
+                table: "EntrepreneurMentoringRequests");
+        }
+    }
+}
