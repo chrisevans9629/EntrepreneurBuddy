44ebeddd342634d21220488a8218610e7565594b
Lydia Winters <lydia@dev-squared.com>
2019-10-27 14:04:51 -0500

    checkmarks

--- EntrepreneurBuddy/src/components/MentoringRequests.vue
+++ EntrepreneurBuddy/src/components/MentoringRequests.vue
@@ -1,157 +1,160 @@
 <style scoped>
-    .mentor-request-card {
-        background-color: #F5F4F4;
-    }
+  .mentor-request-card {
+    background-color: #F5F4F4;
+  }
 
-    .card-img {
-        position: relative;
-        align-self: auto;
-        width: 400px;
-        height: 300px;
-        background-position: 50% 50%;
-        background-repeat: no-repeat;
-        /* background-size: cover; */
-        object-fit: cover;
-    }
+  .card-img {
+    position: relative;
+    align-self: auto;
+    width: 400px;
+    height: 300px;
+    background-position: 50% 50%;
+    background-repeat: no-repeat;
+    /* background-size: cover; */
+    object-fit: cover;
+  }
+
+  .li-height {
+    height: 20px;
+  }
 </style>
 
 <template>
 
-    <div>
-        <div v-if="loading"><img src="/images/loader.gif" alt=""></div>
-        <div v-else>
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-md-4">
-                        <img class="card-img img-fluid" :src="mentor.imageUrl">
-                    </div>
+  <div>
+    <div v-if="loading"><img src="/images/loader.gif" alt=""></div>
+    <div v-else>
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-4">
+            <img class="card-img img-fluid" :src="mentor.imageUrl">
+          </div>
 
-                    <div class="col-md-8">
-                        <div class="ml-2 mt-3">
-                            <h3 class="name-color font-weight-bold">{{mentor.firstName}} {{mentor.lastName}} | {{mentor.position}}</h3>
-                            <p>{{mentor.bio}}</p>
-                            <div class="badge badge-pill text-white badge-blue mr-2 mb-4" v-for="skill in mentor.skillsList" :key="skill">{{skill}}</div>
-                            <div class="d-flex">
-                                <p>{{mentor.rating}}</p>
-                                <img src="/images/thumbs-up-solid.png" class="thumb ml-2" />
-                            </div>
-                            <div class="d-flex" v-if="mentor.linkedInUrl != null">
-                                <a :href="mentor.linkedInUrl"><img src="/images/li.png" class="ml-3" style="width:50px;height:50px" /></a>
-                                <p>Connect with {{mentor.firstName}} on LinkedIn!</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="card mt-5">
-                    <div class="border navbar-custom tutor-request-card py-3">
-                        <div class="pl-2 align-content-center">
-                            <span class="h1 pl-2 text-white">Requests</span>
-                            <a href="#" class="button-light mx-5 float-right" @click="launchCreateRequestModal()" v-if="ismentor=='False'">+ Add Request</a>
-                        </div>
-                    </div>
-                    <div class="row mx-0">
-                        <div v-for="request in mentoringRequests" class="col-md-4 p-4 " :key="request.id">
-                            <request-card :request="request" :ismentor="ismentor" :getMentoringRequests="getMentoringRequests" :currentMentor="currentMentor"></request-card>
-                        </div>
-                    </div>
-                </div>
-
+          <div class="col-md-8">
+            <div class="ml-2 mt-3">
+              <h3 class="name-color font-weight-bold">{{mentor.firstName}} {{mentor.lastName}} | {{mentor.position}}</h3>
+              <p>{{mentor.bio}}</p>
+              <div class="badge badge-pill text-white badge-blue mr-2 mb-4" v-for="skill in mentor.skillsList" :key="skill">{{skill}}</div>
+              <div class="d-flex">
+                <p>{{mentor.rating}}</p>
+                <img src="/images/thumbs-up-solid.png" class="thumb ml-2" />
+                <a :href="mentor.linkedInUrl"><img src="/images/li.png" class="ml-3 li-height" /></a>
+                <p>Connect with {{mentor.firstName}} on LinkedIn!</p>
+              </div>
 
             </div>
+          </div>
         </div>
 
-        <modal name="request-modal" height="auto" :scrollable="true">
-            <div class="px-5 py-3">
-                <div class="row">
-                    <div class="col-12">
-                        <h4>Add Request</h4>
-                    </div>
-                </div>
-                <div class="row mt-3">
-                    <div class="col-12">
-                        <input class="form-control" placeholder="Request Topic" v-model="requestForm.topic">
-                    </div>
-                </div>
-                <div class="row mt-3">
-                    <div class="col-2 offset-8">
-                        <button type="button" class="button" @click="$modal.hide('request-modal')">Cancel</button>
-                    </div>
-                    <div class="col-2">
-                        <button type="button" class="button" @click="submitRequest">
-                            Submit
-                        </button>
-                    </div>
-                </div>
+        <div class="card mt-5">
+          <div class="border navbar-custom tutor-request-card py-3">
+            <div class="pl-2 align-content-center">
+              <span class="h1 pl-2 text-white">Requests</span>
+              <a href="#" class="button-light mx-5 float-right" @click="launchCreateRequestModal()" v-if="ismentor=='False'">+ Add Request</a>
             </div>
-        </modal>
+          </div>
+          <div class="row mx-0">
+            <div v-for="request in mentoringRequests" class="col-md-4 p-4 " :key="request.id">
+              <request-card :request="request" :ismentor="ismentor" :getMentoringRequests="getMentoringRequests" :currentMentor="currentMentor"></request-card>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
     </div>
+
+    <modal name="request-modal" height="auto" :scrollable="true">
+      <div class="px-5 py-3">
+        <div class="row">
+          <div class="col-12">
+            <h4>Add Request</h4>
+          </div>
+        </div>
+        <div class="row mt-3">
+          <div class="col-12">
+            <input class="form-control" placeholder="Request Topic" v-model="requestForm.topic">
+          </div>
+        </div>
+        <div class="row mt-3">
+          <div class="col-2 offset-8">
+            <button type="button" class="button" @click="$modal.hide('request-modal')">Cancel</button>
+          </div>
+          <div class="col-2">
+            <button type="button" class="button" @click="submitRequest">
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </modal>
+  </div>
 
 </template>
 
 <script>
-    import axios from 'axios';
+  import axios from 'axios';
 
-    export default {
-        name: 'MentoringRequests',
-        data: () => ({
-            mentoringRequests: [],
-            loading: false,
-            currentUser: null,
-            currentMentor: null,
-            requestForm: {
-                topic: '',
-                mentorId: 0
-            }
-        }),
-        props: {
-            mentor: {
-                type: Object,
-                default: () => { }
-            },
-            ismentor: {
-                type: String,
-                default: 'False'
-            }
-        },
+  export default {
+    name: 'MentoringRequests',
+    data: () => ({
+      mentoringRequests: [],
+      loading: false,
+      currentUser: null,
+      currentMentor: null,
+      requestForm: {
+        topic: '',
+        mentorId: 0
+      }
+    }),
+    props: {
+      mentor: {
+        type: Object,
+        default: () => { }
+      },
+      ismentor: {
+        type: String,
+        default: 'False'
+      }
+    },
 
-        mounted() {
-            this.getMentoringRequests();
-            this.getUser();
-            this.getMentors();
-        },
+    mounted() {
+      this.getMentoringRequests();
+      this.getUser();
+      this.getMentors();
+    },
 
-        methods: {
-            async getMentoringRequests() {
-                this.loading = true;
-                const { data } = await axios.get('/api/MentoringRequests/' + this.mentor.id);
-                this.mentoringRequests = data;
-                this.loading = false;
-            },
+    methods: {
+      async getMentoringRequests() {
+        this.loading = true;
+        const { data } = await axios.get('/api/MentoringRequests/' + this.mentor.id);
+        this.mentoringRequests = data;
+        this.loading = false;
+      },
 
-            async getUser() {
-                const { data } = await axios.get('/api/Entrepenuers/Current/');
-                this.currentUser = data;
-            },
-            async getMentors() {
-                const { data } = await axios.get('/api/Mentors/Current/');
-                this.currentMentor = data;
-            },
+      async getUser() {
+        const { data } = await axios.get('/api/Entrepenuers/Current/');
+        this.currentUser = data;
+      },
+      async getMentors() {
+        const { data } = await axios.get('/api/Mentors/Current/');
+        this.currentMentor = data;
+      },
 
-            async launchCreateRequestModal() {
-                this.requestForm.topic = '';
-                this.$modal.show('request-modal');
-            },
+      async launchCreateRequestModal() {
+        this.requestForm.topic = '';
+        this.$modal.show('request-modal');
+      },
 
-            async submitRequest() {
-                this.requestForm.mentorId = this.mentor.id;
-                this.loading = true;
-                await axios.post('/api/mentoringrequests/', this.requestForm);
-                this.$modal.hide('request-modal');
-                await this.getMentoringRequests();
-            }
+      async submitRequest() {
+        this.requestForm.mentorId = this.mentor.id;
+        this.loading = true;
+        await axios.post('/api/mentoringrequests/', this.requestForm);
+        this.$modal.hide('request-modal');
+        await this.getMentoringRequests();
+      }
 
 
-        },
-    };
+    },
+  };
 </script>
--- EntrepreneurBuddy/src/components/RequestCard.vue
+++ EntrepreneurBuddy/src/components/RequestCard.vue
@@ -1,174 +1,176 @@
 <style scoped>
-    .card {
-        border: 1px #efefef solid;
-        transition-timing-function: ease-in;
-        transition-property: box-shadow;
-        transition-duration: .1s;
-        transition-timing-function: linear;
-        text-align: left;
-        padding: 10px;
-        background-color: #F5F4F4;
-        border-radius: 15px;
-        width: 100%;
-    }
+  .card {
+    border: 1px #efefef solid;
+    transition-timing-function: ease-in;
+    transition-property: box-shadow;
+    transition-duration: .1s;
+    transition-timing-function: linear;
+    text-align: left;
+    padding: 10px;
+    background-color: #F5F4F4;
+    border-radius: 15px;
+    width: 100%;
+  }
 
-    .button {
-        background-color: #24305e;
-        -moz-border-radius: 17px;
-        -webkit-border-radius: 17px;
-        border-radius: 17px;
-        display: inline-block;
-        cursor: pointer;
-        color: #fff;
-        font-family: Arial;
-        font-size: 17px;
-        font-weight: bold;
-        padding: 5px 10px;
-        text-decoration: none;
-        line-height: 30px;
-    }
+  .button {
+    background-color: #24305e;
+    -moz-border-radius: 17px;
+    -webkit-border-radius: 17px;
+    border-radius: 17px;
+    display: inline-block;
+    cursor: pointer;
+    color: #fff;
+    font-family: Arial;
+    font-size: 17px;
+    font-weight: bold;
+    padding: 5px 10px;
+    text-decoration: none;
+    line-height: 30px;
+  }
 
-    .button-light {
-        background-color: #91b3be;
-        -moz-border-radius: 17px;
-        -webkit-border-radius: 17px;
-        border-radius: 17px;
-        display: inline-block;
-        cursor: pointer;
-        color: #fff;
-        font-family: Arial;
-        font-size: 17px;
-        font-weight: bold;
-        /* padding: 5px 5px; */
-        text-decoration: none;
-        line-height: 30px;
-    }
+  .button-light {
+    background-color: #91b3be;
+    -moz-border-radius: 17px;
+    -webkit-border-radius: 17px;
+    border-radius: 17px;
+    display: inline-block;
+    cursor: pointer;
+    color: #fff;
+    font-family: Arial;
+    font-size: 17px;
+    font-weight: bold;
+    /* padding: 5px 5px; */
+    text-decoration: none;
+    line-height: 30px;
+  }
 
-    .chevron-down {
-        height: 20px;
-    }
+  .chevron-down {
+    height: 20px;
+  }
 
-    .bg-white {
-        background-color: white;
-    }
+  .bg-white {
+    background-color: white;
+    border-radius: 15px;
+  }
 
-    .custom-control-label::before,
-    .custom-control-label::after {
-        top: .8rem;
-        width: 1.25rem;
-        height: 1.25rem;
-    }
+
+  .check {
+    height: 30px;
+  }
 </style>
 
 
 <template>
-    <div class="card shadow-sm ">
-        <div class="m-2">
-            <p><b> {{request.request.topic}}</b></p>
-            <div class="row float-right name-color" style="padding:10px 10px;">
+  <div class="card shadow-sm ">
+    <div class="m-2">
+      <p><b> {{request.request.topic}}</b></p>
+      <div class="row text-left name-color" style="padding:10px 10px;">
 
-                <img src="/images/chevron-down.png" class="chevron-down mr-4 mt-2" v-if="request.emails" @click="showEmails">
-                <div>
-                    <h3 class="pr-3">{{request.attendCount}} interested</h3>
-                </div>
+        <img src="/images/chevron-down.png" class="chevron-down mr-4 mt-2" v-if="request.emails" v-show="!emailsShowing" @click="showEmails">
+        <img src="/images/chevron-up.png" class="chevron-down mr-4 mt-2" v-if="request.emails" v-show="emailsShowing" @click="showEmails">
+        <div>
+          <h3 class="text-left mr-3">{{request.attendCount}} interested</h3>
+        </div>
 
-                <a href="javascript:void(0)" class="button-light" @click="joinHelpRequest()" v-if="ismentor=='False' && request.isJoined != true"> + Join</a>
-                <div class="custom-control form-control-lg custom-checkbox" v-if="ismentor=='False' && request.isJoined == true">
-                    <input type="checkbox" class="custom-control-input" id="customCheck1" checked="checked">
-                    <label class="custom-control-label" for="customCheck1">Joined!</label>
-                </div>
-                <div class="black-text mt-3 bg-white p-2 rounded" v-for="email in request.emails" v-if="emailsShowing">
-                    {{email}}
-                </div>
-                <div class="col-5 ml-3">
-                    <a href="javascript:void(0)" class="button-light" @click="completeHelpRequest()" v-if="ismentor=='True' && currentMentor != null && currentMentor.id == request.request.mentorId"> Complete</a>
-                </div>
-                <div class="col-5 ml-3">
-                    <a href="javascript:void(0)" class="button-light" @click="copyEmails()" v-if="ismentor=='True' && currentMentor != null && currentMentor.id == request.request.mentorId"> {{copy}}</a>
-                </div>
-            </div>
-
+        <a href="javascript:void(0)" class="button-light text-left" @click="joinHelpRequest()" v-if="ismentor=='False' && request.isJoined != true"> + Join</a>
+        <div v-if="ismentor=='False' && request.isJoined == true">
+          <img src="/images/check.png" class="check">
 
         </div>
 
+        <div class="black-text mt-3 bg-white p-4 text-left" v-if="emailsShowing">
+          <a href="javascript:void(0)" class="button-light mb-2" @click="copyEmails()" v-if="ismentor=='True' && currentMentor != null && currentMentor.id == request.request.mentorId && emailsShowing"> {{copy}}</a>
+          <div v-for="email in request.emails">
+            {{email}}
+          </div>
+        </div>
 
-        <modal name="confirm-modal">
-            <div class="m-5 text-center">
-                <p class="text-center">You have been added to this help request!</p>
-                <button type="button" class="button" @click="closeModal">
-                    OK!
-                </button>
-            </div>
-        </modal>
+        <div class=" text-center mt-3">
+          <a href="javascript:void(0)" class="button-light" @click="completeHelpRequest()" v-if="ismentor=='True' && currentMentor != null && currentMentor.id == request.request.mentorId"> Complete</a>
+        </div>
+      </div>
+
+
     </div>
+
+
+    <modal name="confirm-modal">
+      <div class="m-5 text-center">
+        <p class="text-center">You have been added to this help request!</p>
+        <button type="button" class="button" @click="closeModal">
+          OK!
+        </button>
+      </div>
+    </modal>
+  </div>
 
 </template>
 
 <script>
-    import axios from 'axios';
+  import axios from 'axios';
 
-    export default {
-        name: 'RequestCard',
-        data: () => ({
-            emailsShowing: false,
-            copy: 'Copy'
-        }),
+  export default {
+    name: 'RequestCard',
+    data: () => ({
+      emailsShowing: false,
+      copy: 'Copy'
+    }),
 
-        props: {
-            request: {
-                type: Object,
-                default: () => { }
-            }, ismentor: {
-                type: String,
-                default: 'False'
-            }, getMentoringRequests: {
-                type: Function,
-                default: () => { }
-            }, currentMentor: {
-                type: Object,
-                default: () => { }
-            }
-        },
-
-
-        methods: {
-            async joinHelpRequest() {
-                await axios.post('/api/Entrepenuers/Join/' + this.request.request.id);
-                this.request.attendCount++;
-                this.request.isJoined = true;
-                this.$modal.show('confirm-modal');
-            },
-            async completeHelpRequest() {
-                await axios.delete('/api/MentoringRequests/' + this.request.request.id);
-                this.getMentoringRequests();
-            },
-            async closeModal() {
-                this.$modal.hide('confirm-modal');
-            },
-            copyEmails() {
-                var str = this.request.emails.reduce(function reduceFunc(result, value) { return result + ';' + value; });
-                const el = document.createElement('textarea');
-                el.value = str;
-                document.body.appendChild(el);
-                el.select();
-                document.execCommand('copy');
-                document.body.removeChild(el);
-                this.copy = 'Copied!';
-            },
-
-            async showEmails() {
-                this.emailsShowing = !this.emailsShowing;
-                //if (this.showEmails) {
-                //    this.showEmails = false
-                //}
-                //else {
-                //    this.showEmails = true
-                //}
-            }
-        },
+    props: {
+      request: {
+        type: Object,
+        default: () => { }
+      }, ismentor: {
+        type: String,
+        default: 'False'
+      }, getMentoringRequests: {
+        type: Function,
+        default: () => { }
+      }, currentMentor: {
+        type: Object,
+        default: () => { }
+      }
+    },
 
 
+    methods: {
+      async joinHelpRequest() {
+        await axios.post('/api/Entrepenuers/Join/' + this.request.request.id);
+        this.request.attendCount++;
+        this.request.isJoined = true;
+        this.$modal.show('confirm-modal');
+      },
+      async completeHelpRequest() {
+        await axios.delete('/api/MentoringRequests/' + this.request.request.id);
+        this.getMentoringRequests();
+      },
+      async closeModal() {
+        this.$modal.hide('confirm-modal');
+      },
+      copyEmails() {
+        var str = this.request.emails.reduce(function reduceFunc(result, value) { return result + ';' + value; });
+        const el = document.createElement('textarea');
+        el.value = str;
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+        this.copy = 'Copied!';
+      },
 
-    }
+      async showEmails() {
+        this.emailsShowing = !this.emailsShowing;
+        //if (this.showEmails) {
+        //    this.showEmails = false
+        //}
+        //else {
+        //    this.showEmails = true
+        //}
+      }
+    },
+
+
+
+  }
 
 </script>
